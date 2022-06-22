ae87641614a6a46e387be90d51db9989a5ebfb2e
Dan Sazonov <p.294803@gmail.com>
2022-06-22 18:21:55 +0300

    Демка на chart.js

--- src/js/graphs.js
+++ src/js/graphs.js
@@ -1,4 +1,4 @@
-
+import Chart from 'chart.js/auto';
 
 window.Promise ||
 document.write('<script src="https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js"><\/script>');
@@ -8,18 +8,40 @@ document.write('<script src="https://cdn.jsdelivr.net/npm/eligrey-classlist-js-p
 window.Promise ||
 document.write('<script src="https://cdn.jsdelivr.net/npm/findindex_polyfill_mdn"><\/script>');
 
-let packs_num = [0];
-let vals = {
-  height: [0],
-  press: [0],
-  acc: [0],
-  temp: [0]
-};
 
 
+let chartF = new Chart(document.getElementById("height_graph"), {
+  type: 'line',
+  data: {
+    labels: [0],
+    datasets: [{
+      data: [0],
+      label: "Высота, м",
+      borderColor: "#3e95cd",
+      fill: false
+    }]
+  },
+  options: {
+    title: {
+      display: true,
+      text: 'Высота, м'
+    },
+    animation: {
+      duration: 0 // если включить анимацию, будет красиво, но криво. графики медленно на такой частоте строятся
+    }
+  }
+});
 
-function update_data(pack, height, press, acc, temp) {
 
+function update_data(f, label, data) {
+  let chart = chartF;
+  chart.data.labels.push(label);
+  chart.data.datasets.forEach((dataset) => {
+    dataset.data.push(data);
+  });
+  chart.update();
 }
+
+// update_data(chart, 100, 100)
 
 export {update_data}
